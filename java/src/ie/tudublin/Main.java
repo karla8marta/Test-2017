17f4cf0d19f045df45dce7cc5d03e7b54103012b
karla8marta <95685389+karla8marta@users.noreply.github.com>
2022-04-27 10:30:39 +0100

    j

---
+++ java/src/ie/tudublin/Main.java
@@ -0,0 +1,13 @@
+package ie.tudublin;
+
+public class Main {
+
+    public static void starMap() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new StarMap());
+    }
+
+    public static void main(String[] args) {
+        starMap();
+    }
+}
